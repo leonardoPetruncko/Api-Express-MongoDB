6f2f0f166c855fc42331081da8f34f89912936e7
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2024-11-06 03:46:43 -0300

    Create routers

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const port = 3000;
 
+const rotas = {
+    "/": "Curso de Node.js",
+    "/livros": "Rota dos livros",
+    "/autores": "Rota autores"
+}
+
 const server = http.createServer((req , res) => {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Curso de Node.js");
+    res.end(rotas[req.url]);
 });
 
 server.listen(port,() => {
